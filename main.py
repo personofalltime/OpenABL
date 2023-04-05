caaf8506882a116bd5202ce08c679ea32783f8ce
Datis Reyhani <66888981+Datman101@users.noreply.github.com>
2023-04-05 22:41:12 +0100

    small fix + debugging tools

--- main.py
+++ main.py
@@ -20,18 +20,19 @@ for i in range(0, 3):
         os.system("echo G0 X" + str(x_vals[i]) + " Y" + str(y_vals[j]) + ">> /tmp/printer")
         vals[i][j]  = adc.read_adc(0)
 
+print(vals)
 
 with open("/home/pi/printer_data/config/printer.cfg") as cfg:
 
     lines = cfg.readlines()
 
     for line in lines:
-        if ("[bed_mesh]" in line):
+        if ("points" in line):
             find = line
     
     if(find != -1):
 
         val = int(lines.index(find))
         
-        newval = lines[0:val] + vals + lines [val+10:-1]
+        newval = lines[0:val+1] + vals + lines [val+8:-1]
 
