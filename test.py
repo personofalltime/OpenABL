5b8783bcaadeb2d3917efa3fce703ea3719ec743
Datis Reyhani <66888981+Datman101@users.noreply.github.com>
2023-04-05 22:52:13 +0100

    testing

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-import board
-import busio
-import Adafruit_ADS1x15 
 import os
 
-
-
-i2c = busio.I2C(board.SCL, board.SDA)
-adc = Adafruit_ADS1x15.ADS1115()
-
-
-
-print(adc.read_adc(0))
-
-print("loading \n\n\n\n")
-
-file = open("/printer_data/config/printer.cfg")
-print(file.readlines)
+os.system("echo G0 Z2")
