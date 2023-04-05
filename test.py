1036cfcb6dd194b1f7078c11fc0a4fee95978e0f
Datis Reyhani <66888981+Datman101@users.noreply.github.com>
2023-04-05 20:25:13 +0100

    testfile added

---
+++ test.py
@@ -0,0 +1,19 @@
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import os
+
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+
+chan = AnalogIn(ads, ADS.P0)
+
+print(ads.read_adc())
+
+print("loading \n\n\n\n")
+
+file = open("/printer_data/config/printer.cfg")
+print(file.readlines())
